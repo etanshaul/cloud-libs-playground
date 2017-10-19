ccac9dbf64199d8b284bbe195c12025ac2cd51b3
Etan Shaul <eshaul@google.com>
2017-10-19 11:53:58 -0400

    cleanup; add missing deps; update datastore seed

--- src/main/java/com/eshaul/cloud/libs/DatastoreService.java
+++ src/main/java/com/eshaul/cloud/libs/DatastoreService.java
@@ -19,8 +19,6 @@ public class DatastoreService {
         Map<String, String> properties = Maps.newHashMap();
         while (results.hasNext()) {
             Entity currentEntity = results.next();
-            System.out.println(currentEntity.getString("key1"));
-            System.out.println(currentEntity.getNames());
             currentEntity.getNames().forEach(name -> properties.put(name, currentEntity.getString(name)));
         }
 
@@ -29,9 +27,9 @@ public class DatastoreService {
 
     void seedData() {
         Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
-        KeyFactory keyFactory = datastore.newKeyFactory().setKind("LocalEmulator");
-        Key key = keyFactory.newKey("localKey") ;
-        Entity entity = Entity.newBuilder(key).set("test", "testVal").build();
+        KeyFactory keyFactory = datastore.newKeyFactory().setKind("TestKind");
+        Key key = keyFactory.newKey("localEmulatorKey") ;
+        Entity entity = Entity.newBuilder(key).set("emKey", "emVal").build();
         datastore.put(entity);
     }
 }
